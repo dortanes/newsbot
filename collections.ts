1a0fbc40940cdd1bf75a19c2572638e662c5ef9f
1devout0 <1devout0@gmail.com>
2023-03-15 20:52:00 +1000

    bb

--- collections.ts
+++
@@ -1,9 +0,0 @@
-import { collection } from "typesaurus";
-import useDatabase from "./database";
-import { Source, NewsItem, Setting } from "./typings/entities";
-
-export default {
-  news: useDatabase<NewsItem>("news"),
-  sources: useDatabase<Source>("sources"),
-  settings: useDatabase<Setting>("settings"),
-};
